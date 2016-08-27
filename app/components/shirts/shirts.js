a86d674b0cd01e886106a8de4ee4cf6a6850b971
Taylor Pennock <taylor.pennock@gmail.com>
2016-08-27 17:42:12 -0600

    apparel and shirts components;

--- app/components/shirts/shirts.js
+++ app/components/shirts/shirts.js
@@ -7,5 +7,26 @@ angular.module('jwrApp.blocks').component('jwrShirts', {
     controller: function JwrShirtsCtrl($log) {
         var $shirts = this;
 
+        $shirts.list = [
+            {
+                name: 'Wardy',
+                price: 20,
+                image: 'img/jwrc/shirts/w1.png',
+                link: ''
+            },
+            {
+                name: 'Jared Ward Running Co.',
+                price: 20,
+                image: 'img/jwrc/shirts/w2.png',
+                link: ''
+            },
+            {
+                name: 'Staches for State',
+                price: 20,
+                image: 'img/jwrc/shirts/w3.png',
+                link: ''
+            }
+        ];
+
     }
 });
